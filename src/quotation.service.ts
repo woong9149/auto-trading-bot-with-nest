b0f281aab9d969796fe3e1260f5d0726faae315b
woong9149 <sewoong0409@naver.com>
2021-10-24 20:07:26 +0900

    211024_2

--- src/quotation.service.ts
+++ src/quotation.service.ts
@@ -5,10 +5,9 @@ export class QuotationService {
 	constructor(private httpService: HttpService) {}
 
 	private readonly DATA_URL = 'https://api.upbit.com/v1/candles/days?market=KRW-BTC&count=1';
-	
+
 	async reqDaysCandles() {
 		const candles = await this.httpService.get(this.DATA_URL).toPromise();
-
 		console.log(candles.data);
 	}
 }
